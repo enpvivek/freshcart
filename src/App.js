12aae4b0bfd4bb53e2eceba94dca18303fe5e3c7
Vivek Yadav <enpvivek@gmail.com>
2022-06-23 16:17:43 +0530

    Created and styled header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { Header, CreateContainer, MainContainer } from "./components";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
-      App
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+
+        <main className="mt-24 p-8 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { MdShoppingCart } from "react-icons/md";
+import { motion } from "framer-motion";
+
+import Logo from "../img/logo.png";
+import Avatar from "../img/avatar.png";
+
+const Header = () => {
+  return (
+    <header className="fixed w-screen z-50  p-6 px-16">
+      {/* for Desktop and Tablet  */}
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img className="w-8 object-cover" src={Logo} alt="logo" />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </div>
+
+        <div className="flex items-center gap-8">
+          <ul className="flex ml-auto gap-8 items-center">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Home
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Menu
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              About Us
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Service
+            </li>
+          </ul>
+
+          <div className="relative flex items-center justify-center ">
+            <MdShoppingCart className="text-textColor text-2xl cursor-pointer" />
+            <div className="absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-sm text-white font-semibold">2</p>
+            </div>
+          </div>
+          <motion.img whileTap={{scale: 0.6}}
+            src={Avatar}
+            alt="userProfile"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
+          />
+        </div>
+      </div>
+      {/* for Mobile Users  */}
+      <div className="flex md:hidden w-full h-full"></div>
+    </header>
+  );
+};
+
+export default Header;
