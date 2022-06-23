c04748864c744dd9603c29d0b6e9b6f3243c3b88
Vivek Yadav <enpvivek@gmail.com>
2022-06-24 01:20:02 +0530

    Added Login authentication and styled HomePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdShoppingCart } from "react-icons/md";
+import { MdShoppingCart, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from "framer-motion";
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -8,18 +8,44 @@ import { app } from "../firebase.config";
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
 import { Link } from "react-router-dom";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
+import { useState } from "react";
 
 const Header = () => {
-   const firebaseAuth = getAuth(app);
-   const provider = new GoogleAuthProvider();
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
-  const login = async () =>{
-    const response = await signInWithPopup(firebaseAuth, provider);
-    console.log(response);
-  }
+  const [{ user }, dispatch] = useStateValue();
+
+  const [isMenu, setIsMenu] = useState(false);
+
+  const login = async () => {
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
+    }
+  };
+
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
 
   return (
-    <header className="fixed w-screen z-50  p-6 px-16">
+    <header className="fixed w-screen z-50 p-3 px-4 md:p-6 md:px-16">
       {/* for Desktop and Tablet  */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
@@ -28,7 +54,12 @@ const Header = () => {
         </Link>
 
         <div className="flex items-center gap-8">
-          <ul className="flex ml-auto gap-8 items-center">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex ml-auto gap-8 items-center"
+          >
             <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
               Home
             </li>
@@ -41,7 +72,7 @@ const Header = () => {
             <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
               Service
             </li>
-          </ul>
+          </motion.ul>
 
           <div className="relative flex items-center justify-center ">
             <MdShoppingCart className="text-textColor text-2xl cursor-pointer" />
@@ -53,16 +84,100 @@ const Header = () => {
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
-              src={Avatar}
+              src={user ? user.photoURL : Avatar}
               onClick={login}
               alt="userProfile"
-              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
             />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+              >
+                {user && user.email === "enpvivek@gmail.com" && (
+                  <Link to={"/createItem"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  onClick={logout}
+                >
+                  Logout <MdLogout></MdLogout>
+                </p>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
+
       {/* for Mobile Users  */}
-      <div className="flex md:hidden w-full h-full"></div>
+      <div className="flex item-center justify-between md:hidden w-full h-full">
+        <div className="relative flex items-center justify-center ">
+          <MdShoppingCart className="text-textColor text-2xl cursor-pointer" />
+          <div className="absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-sm text-white font-semibold">2</p>
+          </div>
+        </div>
+
+        <Link to={"/"} className="flex items-center gap-2">
+          <img className="w-8 object-cover" src={Logo} alt="logo" />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </Link>
+
+        <div className="relative">
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={user ? user.photoURL : Avatar}
+            onClick={login}
+            alt="userProfile"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+            >
+              {user && user.email === "enpvivek@gmail.com" && (
+                <Link to={"/createItem"}>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    New Item <MdAdd />
+                  </p>
+                </Link>
+              )}
+
+              <ul className="flex flex-col ">
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer px-4 py-2 hover:bg-slate-100">
+                  Home
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer px-4 py-2 hover:bg-slate-100">
+                  Menu
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer px-4 py-2 hover:bg-slate-100">
+                  About Us
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer px-4 py-2 hover:bg-slate-100">
+                  Service
+                </li>
+              </ul>
+
+              <p
+                className="m-2 p-2 rounded-md shadow-md flex items-center gap-3 cursor-pointer bg-gray-200 hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
+                onClick={logout}
+              >
+                Logout <MdLogout></MdLogout>
+              </p>
+            </motion.div>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+
+import Delivery from "../img/delivery.png";
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
+            Bike Delivery
+          </p>
+          <div className="w-6 h-6 rounded-full bg-white overflow-hidden drop-shadow-xl">
+            <img
+              src={Delivery}
+              alt=""
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
 
-export default MainContainer
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The fastest delivery in{" "}
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+            Your City
+          </span>
+        </p>
+        <p className="text-base text-textColor text-center md:text-left md:w-80%">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
+          aspernatur voluptate voluptatibus nisi fuga dicta eveniet, aliquid
+          perferendis cupiditate nesciunt in molestias ipsum iste impedit quae
+          tempora beatae neque nam?
+        </p>
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
+      </div>
+      <div className="py-2 flex-1"></div>
+    </div>
+  );
+};
+
+export default MainContainer;
